2fe4fb1ee0f9df7913a1ee303951eee46c7d0402
VisedFeline121 <ororp3@gmail.com>
2025-10-08 22:26:32 +0300

    added init files

---
+++ app/api/schemas/__init__.py
@@ -0,0 +1,37 @@
+"""API schemas package."""
+
+from .chat import (
+    AddMemberRequest,
+    ChatCreatedResponse,
+    ChatListResponse,
+    ChatMembersResponse,
+    ChatResponse,
+    ChatType,
+    CreateChatRequest,
+    DirectMessageResponse,
+    ErrorResponse,
+    GroupChatResponse,
+    MemberAddedResponse,
+    MemberRole,
+    MembershipResponse,
+    SuccessResponse,
+    UserResponse,
+)
+
+__all__ = [
+    "AddMemberRequest",
+    "ChatCreatedResponse",
+    "ChatListResponse",
+    "ChatMembersResponse",
+    "ChatResponse",
+    "ChatType",
+    "CreateChatRequest",
+    "DirectMessageResponse",
+    "ErrorResponse",
+    "GroupChatResponse",
+    "MemberAddedResponse",
+    "MemberRole",
+    "MembershipResponse",
+    "SuccessResponse",
+    "UserResponse",
+]

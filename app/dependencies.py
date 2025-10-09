54ab34a6044960203a1cf9e6ce3c1efc71da7166
VisedFeline121 <ororp3@gmail.com>
2025-10-09 21:56:30 +0300

    added message and repo dependancies

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,8 +2,10 @@
 
 from app.db.db import get_session_local
 from app.repositories.chat_repo import ChatRepo
+from app.repositories.message_repo import MessageRepo
 from app.repositories.user_repo import UserRepo
 from app.services.chat_service import ChatService
+from app.services.message_service import MessageService
 
 
 def get_user_repo() -> UserRepo:
@@ -21,3 +23,15 @@ def get_chat_service() -> ChatService:
     chat_repo = get_chat_repo()
     user_repo = get_user_repo()
     return ChatService(chat_repo, user_repo)
+
+
+def get_message_repo() -> MessageRepo:
+    """Get MessageRepo instance with session factory."""
+    return MessageRepo(get_session_local())
+
+
+def get_message_service() -> MessageService:
+    """Get MessageService instance with dependencies."""
+    message_repo = get_message_repo()
+    chat_repo = get_chat_repo()
+    return MessageService(message_repo, chat_repo)
